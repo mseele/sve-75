503cde1c2d79a1499d3e0379b96d0a34d1737fb4
Michael Seele <mseele@gmail.com>
2022-04-16 11:17:23 +0200

    chore: remove unused tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,38 +11,7 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            a: {
-              color: theme('colors.red.800'),
-              'border-radius': '0.25rem',
-              '&:focus': {
-                outline: '2px solid transparent',
-                'outline-offset': '2px',
-              },
-              '&:focus-visible': {
-                '--tw-ring-offset-shadow':
-                  'var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color)',
-                '--tw-ring-shadow':
-                  'var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color)',
-                'box-shadow':
-                  'var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000)',
-                '--tw-ring-opacity': '1',
-                '--tw-ring-color':
-                  'rgba(168, 162, 158, var(--tw-ring-opacity))',
-                '--tw-ring-opacity': '0.5',
-                '--tw-ring-offset-width': '2px',
-              },
-            },
-          },
-        },
-      }),
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('prettier-plugin-tailwindcss'),
-  ],
+  plugins: [require('prettier-plugin-tailwindcss')],
 }
